9bd6899994a89a458b7664c75b635e8a63aaa678
OBrien-T <obrienadedapo15@gmail.com>
2023-05-26 12:20:07 +0100

    added a lil bit of Javascript

--- showPopUp.js
+++ showPopUp.js
@@ -3,3 +3,8 @@ function showPopUpBig() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+// const hidePopUp = () => {
+//     var popup = document.getElementById("myPopup");
+//     popup.classList.toggle("hide")
+// }
